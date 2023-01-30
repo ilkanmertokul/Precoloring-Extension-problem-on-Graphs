aa18387b60de9e1abf7616ea5f65a35ff0c77680
ilkanmertokul <ilkanmertokul@gmail.com>
2023-01-30 22:35:36 +0300

    Last version, added coloring and time measure

--- Test_codes/DSatur_test.py
+++ Test_codes/DSatur_test.py
@@ -1,17 +1,8 @@
 import graph_operations
 
-a = graph_operations.GraphOperator.from_random_generator(5000, 0.5)
+a = graph_operations.GraphOperator.from_random_generator(100, 0.2)
 
 a.assign_color_to_node(1, 2)
-#print(a.)
-#print(a.)
-
+a.assign_color_to_node(2, 3)
 
 a.dsatur_Algorithm()
-
-#for i in range(0,a.node_count):
-#    for j in a.graph.neighbors(i):
-#        print(f"{i} {j}")
-
-#for i in range(0,a.node_count):
-#    print(f"{i} - {a.graph.nodes[i]}")
